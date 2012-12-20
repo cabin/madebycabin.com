2bbefb2eebb6838cdbb0fe15465e6b1cc5e42fc1
Zak Johnson <zakj@nox.cx>
2012-12-19 21:57:11 -0800

    Add logo assets and some stub routes and styles

--- cabin/main.py
+++ cabin/main.py
@@ -6,3 +6,18 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/work')
+def work():
+    return 'work'
+
+
+@main.route('/about')
+def about():
+    return 'about'
+
+
+@main.route('/lab')
+def lab():
+    return 'lab'
