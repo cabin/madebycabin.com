72fd6be18ceabf9f9979c53d1f0ef7f4b87f51f7
Zak Johnson <zakj@nox.cx>
2012-12-29 22:52:30 -0800

    Add a ValidatedHash helper and tests

---
+++ cabin/util/validated_hash.py
@@ -0,0 +1,100 @@
+import collections
+import json
+
+import jsonschema
+
+
+# As we're stuck with only Redis for now, this hack provides a data access
+# layer.
+class ValidatedHash(object):
+    """A simple wrapper around objects stored as Redis hashes.
+
+    Each `ValidatedHash` subclass must have a JSON schema (in the form of a
+    dictionary) `schema` attribute, which is used to decode the Redis hash
+    members (which are stored as strings) appropriately.
+    
+    Subclasses can provide `_decode_<type>` methods for custom decoding of
+    arbitrary JSON schema types.
+
+    The primary interface is via the `decode` method, which can be passed the
+    results of a Redis `HGETALL` command.
+
+    """
+    type_map = {
+        'array': list,
+        'boolean': bool,
+        'integer': int,
+        'object': dict,
+        'string': str,
+    }
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    @classmethod
+    def decode(cls, data):
+        return cls(**cls._decode_object(data, cls.schema))
+
+    @classmethod
+    def _get_decoder(cls, _type):
+        return getattr(cls, '_decode_%s' % _type, cls._decode_default)
+
+    @classmethod
+    def _decode_default(cls, value, schema):
+        _type = cls.type_map[schema['type']]
+        if value is None:
+            if 'default' in schema:
+                value = schema['default']
+            else:
+                value = _type()
+        return value
+
+    @classmethod
+    def _decode_array(cls, value, schema):
+        value = cls._decode_default(value, schema)
+        item_schema = schema.get('items')
+        if not item_schema:
+            return value
+        decoder = cls._get_decoder(item_schema['type'])
+        arr = []
+        for item in value:
+            arr.append(decoder(item, item_schema))
+        return arr
+
+    @classmethod
+    def _decode_boolean(cls, value, schema):
+        value = cls._decode_default(value, schema)
+        return value in {True, '1', 'True', 'true'}
+
+    @classmethod
+    def _decode_integer(cls, value, schema):
+        value = cls._decode_default(value, schema)
+        return cls.type_map['integer'](value)
+
+    @classmethod
+    def _decode_object(cls, value, schema):
+        obj = {}
+        for name, schema in schema['properties'].items():
+            item = value.get(name)
+            # A leading underscore indicates a JSON-encoded object (necessary
+            # since we can't nest hashes in Redis). We drop the underscore on
+            # the output object for API convenience.
+            if name.startswith('_'):
+                name = name[1:]
+                if item is not None:
+                    item = json.loads(item)
+            item = cls._get_decoder(schema['type'])(item, schema)
+            obj[name] = item
+        return obj
+
+    @property
+    def errors(self):
+        validator = jsonschema.Draft3Validator(self.schema)
+        errors = collections.defaultdict(list)
+        for error in validator.iter_errors(self.__dict__):
+            path = '.'.join(reversed(error.path))
+            errors[path].append(error.message)
+        return errors
+
+    def is_valid(self):
+        return not bool(self.errors)
---
+++ tests/test_validated_hash.py
@@ -0,0 +1,102 @@
+import pytest
+
+from cabin.util.validated_hash import ValidatedHash
+
+
+class Empty(ValidatedHash):
+    schema = {
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {}
+    }
+
+
+class Defaults(ValidatedHash):
+    schema = {
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {
+            'boolF': {'type': 'boolean'},
+            'boolT': {'type': 'boolean', 'default': True},
+            'string': {'type': 'string'},
+            'int': {'type': 'integer'},
+            'stringSpam': {'type': 'string', 'default': 'spam'},
+            'array': {'type': 'array'},
+        }
+    }
+
+
+class Nested(ValidatedHash):
+    schema = {
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {
+            'name': {'type': 'string', 'required': True},
+            'intList': {
+                'type': 'array',
+                'items': {'type': 'integer'},
+            },
+            'menu': {
+                'type': 'array',
+                'uniqueItems': True,
+                'items': {
+                    'type': 'object',
+                    'properties': {
+                        'name': {'type': 'string', 'required': True},
+                        'price': {'type': 'integer', 'required': True},
+                    }
+                }
+            }
+        }
+    }
+
+
+def test_ignore_bad_input():
+    obj = Empty.decode({'no': 'valid', 'properties': 'here'})
+    assert obj.__dict__ == {}
+    assert obj.is_valid() is True
+
+
+def test_default_values():
+    obj = Defaults.decode({})
+    assert obj.boolF == False
+    assert obj.boolT == True
+    assert obj.string == ''
+    assert obj.stringSpam == 'spam'
+    assert obj.array == []
+    # Ensure that defaults don't *always* override decoded values.
+    obj = Defaults.decode(
+        {'boolF': True, 'boolT': False, 'string': 'foo', 'stringSpam': 'bar'})
+    assert obj.boolF == True
+    assert obj.boolT == False
+    assert obj.string == 'foo'
+    assert obj.stringSpam == 'bar'
+
+
+def test_coercion():
+    obj = Defaults.decode({
+        'int': '123',
+        'boolF': 'False',
+        'boolT': 'True',
+    })
+    assert obj.int == 123
+    assert obj.boolF == False
+    assert obj.boolT == True
+
+
+def test_nested_object():
+    obj = Nested.decode({})
+    assert obj.menu == []
+    obj = Nested.decode({'menu': [{'name': 'eggs', 'price': '12'}]})
+    assert obj.menu[0]['name'] == 'eggs'
+    assert obj.menu[0]['price'] == 12
+    obj = Nested.decode({'intList': ['1', '2', '3']})
+    assert obj.intList == [1, 2, 3]
+
+
+def test_nested_json():
+    pytest.skip('TODO')
+
+
+def test_validation():
+    pytest.skip('TODO')
