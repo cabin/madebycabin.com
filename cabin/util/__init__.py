9d3c49080eaaa424445fa38f8a21bdf78aab40f1
Zak Johnson <zakj@nox.cx>
2013-01-10 22:59:05 -0800

    Encode strings before handing off to url* functions

--- cabin/util/__init__.py
+++ cabin/util/__init__.py
@@ -4,11 +4,11 @@ import urlparse
 
 def hostname(s):
     if s:
-        return urlparse.urlparse(s).hostname
+        return urlparse.urlparse(s.encode('utf8')).hostname
     return ''
 
 
 def urlquote(s):
     if s:
-        return urllib.quote_plus(s, safe='/')
+        return urllib.quote_plus(s.encode('utf8'), safe='/')
     return ''
