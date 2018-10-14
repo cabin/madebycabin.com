fd552020153cfbdda5b6d97a63fc227ce0173807
Zak Johnson <me@zakj.net>
2018-10-13 19:41:40 -0700

    Add video loop easing

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ import './base.styl';
 const videoEl = document.querySelector('.background');
 const loopStart = 8.1;
 const loopEnd = 11.5;  // videoLength = videoEl.seekable.end(videoEl.seekable.length - 1);
+const loopDuration = (loopEnd - loopStart) * 2;
 let direction = 1;  // 1 for forward, -1 for backward
 let iterationStart;
+
+// Ease in/out when looping to avoid glitchiness, but ease only out on the
+// first pass to maintain linear motion.
+const easeOutQuad = t => t * (2 - t);
+const easeInOutQuad = t => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+let ease = easeOutQuad;
 
 function waitForLoopStart() {
   let fn = waitForLoopStart;
@@ -18,9 +25,12 @@ function waitForLoopStart() {
 
 function loopVideo() {
   const now = performance.now();
-  const delta = (now - iterationStart) / 1000 * direction;
-  const newTime = (direction > 0 ? loopStart : loopEnd) + delta;
-  if (newTime >= loopEnd || newTime <= loopStart) {
+  const delta = (now - iterationStart) / 1000;
+  const t = delta / loopDuration;
+  const easedDelta = ease(t) * (loopEnd - loopStart);
+  const newTime = (direction > 0 ? loopStart : loopEnd) + easedDelta * direction;
+  if (t >= 1) {
+    ease = easeInOutQuad;
     iterationStart = now;
     direction *= -1;
   }
@@ -36,5 +46,6 @@ videoEl.addEventListener('loadeddata', waitForLoopStart);
 if (window.innerHeight && window.screen && window.screen.availHeight && window.innerHeight < window.screen.availHeight) {
   const resize = () => document.querySelector('main').style.height = window.innerHeight + 'px';
   window.addEventListener('orientationchange', resize);
+  window.addEventListener('resize', resize);
   resize();
 }
